371c5b428b0e31286b325cbb49de025727a34deb
Thomas William Hamer <u6081614>
2018-04-23 17:31:10 +1000

    add course data endpoint

--- Code/courseai/degree/views.py
+++ Code/courseai/degree/views.py
@@ -6,6 +6,9 @@ from . import degree_plan_helper
 from . import initialiser
 from .models import Degree
 
+
+from . import course_data_helper
+
 # initialiser.initialise_database()
 
 def all_degrees(request):
@@ -25,6 +28,14 @@ def degree_plan(request):
         return degree_plan_helper.generate_degree_plan(code, starting_year)
     except(Exception):
         raise Exception("Please provide a valid degree code and starting year")
+
+def course_data(request):
+
+    try:
+        code=request.GET['query']
+        return JsonResponse({"response": course_data_helper.get_data(code)})
+    except(Exception):
+        raise Exception("Please provide a valid course code")
 
 
 
