1a1f94a3831a416c25f31e7d9b6e7197f66089eb
Thomas William Hamer <u6081614>
2018-04-24 11:46:19 +1000

    update course data
    Former-commit-id: a1a7918fad030ff673b7b323d8063a20fd7aa18a

---
+++ Code/courseai/degree/nn.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+from .course_data_helper import get_all
+
+
+def get_course_vector(code):
+    print(get_all())
+
+def run():
+
+    print(get_course_vector(code=''))
