69d228b4da079f8007d1b6103eb95cbf8fa64f32
Thomas William Hamer <u6081614>
2018-04-28 04:01:31 +1000

    update elective to code rather than title
    Former-commit-id: d970bf8f76690aadb57af033b896f30d43f13333

--- Code/courseai/degree/degree_plan_helper.py
+++ Code/courseai/degree/degree_plan_helper.py
@@ -8,6 +8,12 @@ from .models import Degree
 def advance_sem(year, sem):
     return (year, 2) if sem is 1 else (year + 1, 1)
 
+def update_elective_code(c):
+    if('title' in c):
+        if (c['title'].lower() == "elective course"):
+            c['code'] = "elective course"
+            c.pop('title')
+    return c
 
 def generate_degree_plan(code, start_year_sem):
     degree = Degree.objects.filter(code=code)[0]
@@ -16,6 +22,8 @@ def generate_degree_plan(code, start_year_sem):
     year, sem = start_year_sem.split('S')
     year, sem = int(year), int(sem)
     for year_sem, courses in eval(reqs).items():
+        for c in courses:
+            update_elective_code(c)
         to_return.append({'{}S{}'.format(year, sem): courses})
         year, sem = advance_sem(year, sem)
     return JsonResponse({"response": to_return})
