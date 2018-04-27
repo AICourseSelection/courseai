54ce62720e3b5d425c35a5d0764d2fdc86f6e618
Joseph Meltzer <u6060661@anu.edu.au>
2018-04-27 19:07:02 +1000

    Display Majors, Minors, Specs on RHS
    * Can add Majors, Minors, Specs from LHS search by clicking 'Add to Plan'
    * Each M/M/S and its subsections are collapsible
    * Compulsory courses displayed at the top
    * Each minimum/maximum section is displayed separately
    * Unit requirements are displayed (updating not implemented yet)
    * Course titles are retrieved in bulk from server
      * Currently holds up the main thread. Will change later.
    * Server 'course_data' GET endpoint modified to allow supplying of multiple codes to retrieve just their titles.
      * set 'query' to 'titles'
      * set 'codes' to a JSON string list of course codes whose titles are needed
    * Course items are clickable as usual
    * 'Add to Plan' button is disabled if M/M/S is already in plan
    * Each M/M/S can be deleted from the list. This allows it to be added again from the search.
    * Example of course 'ticking' and colour coding is displayed by default (will be removed eventually)
      * Courses which are currently in the plan are ticked
      * Sections and subsections whose requirements are met are green
      * Sections and subsections whose requirements are not met are yellow
      * Compulsory courses which are not in the plan are shown with a cross
    
    
    Former-commit-id: 5df0503b8419ca03510ef2be66bc5750ae67fc01

--- Code/courseai/degree/course_data_helper.py
+++ Code/courseai/degree/course_data_helper.py
@@ -1,7 +1,10 @@
-from builtins import str, list, eval
+import json
+from builtins import str, eval
+
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Q
+from elasticsearch_dsl import Search
 from elasticsearch_dsl.query import MultiMatch
+
 
 def get_data(code):
     q = MultiMatch(query=code, fields=['code^4'])
@@ -15,13 +18,33 @@ def get_data(code):
         return None
 
     print("****\n", hit, "****\n")
-    course_data = {"course_code":hit['_source']['code'],
-                    "title": hit['_source']['title'],
-                    "description":hit['_source']['description'],
-                    "learning_outcomes":hit['_source']['outcome'],
-                    "prerequisite_text":hit['_source']['prereq_text'],
-                    "prerequisites": eval(str(hit['_source']['pre_req_cnf']))
-    }
+    course_data = {"course_code": hit['_source']['code'],
+                   "title": hit['_source']['title'],
+                   "description": hit['_source']['description'],
+                   "learning_outcomes": hit['_source']['outcome'],
+                   "prerequisite_text": hit['_source']['prereq_text'],
+                   "prerequisites": eval(str(hit['_source']['pre_req_cnf']))
+                   }
+    return course_data
+
+
+def get_titles(codes):
+    course_data = []
+    codes = json.loads(codes)
+    for code in codes:
+
+        q = MultiMatch(query=code, fields=['code^4'])
+        client = Elasticsearch()
+        s = Search(using=client, index='courses')
+        response = s.query(q).execute()
+
+        try:
+            hit = response['hits']['hits'][0]
+        except IndexError:
+            continue
+
+        course_data.append({"course_code": hit['_source']['code'],
+                            "title": hit['_source']['title']})
     return course_data
 
 def get_all():
