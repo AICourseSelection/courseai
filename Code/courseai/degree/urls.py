63369382a77f46c2632c8e41d76093bb16317e48
Joseph Meltzer <u6060661@anu.edu.au>
2018-05-04 02:56:56 +1000

    Degree Requirements and Course Delete
    * Degree requirements appear in RHS panel.
    * One section for each requirement type.
    * Each section shows up green when completed.
    * Each section which contains courses/majors is collapsible.
    * Sections are re-orderable.
    * Sections for choosing a major indicate which ones are complete and/or in progress.
      * Red border when none are in progress.
    * Overall degree unit counter.
    * Can add courses by drag-and-drop.
    * Can add majors by clicking and pressing 'Add to Plan'
    * Required majors are automatically added.
    * New majors appear at the top of the list and not the bottom.
    
    * Can drag courses to first cell in row to delete.
    * Degree and major requirements are recalculated.
    
    * Updated degree requirement JSON files with unit requirements.
    * Renamed some of these files to the correct codes.
    * Created new server endpoint "degree/degreereqs" to serve the requirement JSON file.
    
    
    Former-commit-id: 9180f91ea7212ed3ef0aae3fb838ab0b5da085e2

--- Code/courseai/degree/urls.py
+++ Code/courseai/degree/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('all', views.all_degrees, name='all_degrees'),
     path('degreeplan', views.degree_plan),
+    path('degreereqs', views.degree_reqs),
     path('coursedata', views.course_data),
     path('mms', views.mms_request),
     path('majors', views.all_majors),
