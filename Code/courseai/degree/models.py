49861176ba15ebe9a10023d1ef400868b00cfae4
manalmohania <u6019071@anu.edu.au>
2018-05-05 11:18:15 +1000

    neural nets basics

--- Code/courseai/degree/models.py
+++ Code/courseai/degree/models.py
@@ -6,6 +6,8 @@ class Degree(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=150)
     requirements = models.TextField()
+    number_of_enrolments = models.IntegerField(default=1)
+    metrics = models.TextField(default="{}")
 
     def __str__(self):
         return self.name
