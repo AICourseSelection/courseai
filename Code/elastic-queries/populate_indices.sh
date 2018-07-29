f2151ad1f8935cd3111850dd61b9b2951567cf31
manalmohania <u6019071@anu.edu.au>
2018-07-29 22:25:14 +1000

    added indices poplulation script

---
+++ Code/elastic-queries/populate_indices.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# I haven't yet added the command to clear the indices. Will do when I'm less lazy
+
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@course_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@degree_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@major_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@minor_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@specialisations_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@cbelists_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@courselists_bulk"; echo
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@cse_bulk"; echo
+
+
+
