db5393441207a05724c75e5dd5744cf2287fd3ac
Sijia Qin <u5906375@anu.edu.au>
2018-08-05 19:34:42 +1000

    added a new accounts app which contains user register, login, logout.   Note: User login information are checked, and unsuccessful information display an error message.  Database records user registration data, and password are not stored in plain text. However, form.html is ONLY A DRAFT and is not working yet.

---
+++ Code/courseai/accounts/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
+
+User = get_user_model() # needed for user registration
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField
+    password = forms.CharField(widget=forms.PasswordInput)  # hidden passwords are not stored in plain text
+
+    # when form is doing validation, "form.is_valid()",
+    # check username, password, whether user is registered, whether user is active.
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError("This user does not exist.")
+            if not user.is_active:
+                raise forms.ValidationError("This user is not active.")
+            if not user.check_checkpassword(password):
+                raise forms.ValidationError("This password is incorrect.")
+        return super(UserLoginForm, self).clean(*args, **kwargs)    # return default, not giving field error.
+
+
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(label="Email adress")  # overide default email
+    email2 = forms.EmailField(label="Confirm email")
+
+    class Meta():
+        model = User
+        fields = [    # order matters
+            'username',
+            'email',
+            'email2',
+            'password'
+        ]
+
+    def clean_email2(self):
+        """ Same as 'clean' method but give field error. """
+        email = self.cleaned_data.get("email")
+        email2 = self.cleaned_data.get("confirm_email")
+        if email != email2:
+            raise forms.ValidationError("Emails must match")
+        email_db = User.objects.filter(email = email)
+        if email_db.exists():
+            raise forms.ValidationError("This email has already been registered")
+        return email
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Code/courseai/accounts/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('login_view', views.login_view),
+    path('register_view', views.register_view),
+    path('logout_view', views.logout_view)
+]
