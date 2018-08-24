fc196873a9389d443a3fc03fe9502eda0ffe5f5e
Thien Bui-Nguyen <thien.minh.bui.nguyen@gmail.com>
2018-08-24 21:21:12 +1000

    ajax calls working for login/register on index.html
    Former-commit-id: 457ee51e46ba579100660aadd522236d5158d4dd

--- Code/courseai/accounts/urls.py
+++ Code/courseai/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('login_view', views.login_view),     # http://127.0.0.1:8000/accounts/login_view
-    path('register_view', views.register_view),
+    path(r'^ajax/register_view$', views.register_view, name='register_view'),
     path('logout_view', views.logout_view)
 ]
