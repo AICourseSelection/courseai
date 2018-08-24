457ee51e46ba579100660aadd522236d5158d4dd
Thien Bui-Nguyen <thien.minh.bui.nguyen@gmail.com>
2018-08-24 21:21:12 +1000

    ajax calls working for login/register on index.html

--- Code/courseai/accounts/views.py
+++ Code/courseai/accounts/views.py
@@ -8,6 +8,21 @@ from django.contrib.auth import (
     logout
 )
 
+# decorator function by Mathieu Marques - https://stackoverflow.com/questions/16569784/how-does-one-protect-his-ajax-views-with-django/18213333#18213333
+def require_AJAX(function):
+    """Return a bad request instance if the view is not using AJAX
+    function -- the view
+    """
+
+    def wrap(request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+        return function(request, *args, **kwargs)
+
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
+
 # create form for each aspects of these
 #@csrf_protect
 def login_view(request):
@@ -25,7 +40,7 @@ def login_view(request):
             return redirect("/")
     return render(request, "dynamic_pages/index.html", {"form": form, "title": title})    #(request, template, context dictionary)
 
-#@csrf_protect
+@require_AJAX
 def register_view(request):
     title = "Register"
     form = UserRegisterForm(request.POST or None, request)
@@ -38,13 +53,13 @@ def register_view(request):
         user.save()
 
         login(request, user)
-        return redirect("/")
-    return redirect("/")    
-    #context = {
-    #    "register_form": form,
-    #    "title": title
-    #}
-    #return render(request, "dynamic_pages/index.html", context)
+        return redirect("/")   
+        
+    context = {
+        "form": form,
+        "title": title
+    }
+    return render(request, "dynamic_pages/index.html", context)
        
 #@csrf_protect
 def logout_view(request):
