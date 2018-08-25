d1c337d400089bd91b0d7d9c713ba6cda654da8b
Thien Bui-Nguyen <thien.minh.bui.nguyen@gmail.com>
2018-08-25 22:54:07 +1000

    fix es client to be globalised client in get_multiple

--- Code/courseai/courseai/settings.py
+++ Code/courseai/courseai/settings.py
@@ -28,7 +28,7 @@ CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
