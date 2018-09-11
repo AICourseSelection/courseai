ed10d9caa93c54b3aa8987303c4b7d59a8ecbccb
Thomas William Hamer <u6081614>
2018-09-11 19:10:10 +1000

    add feedback module

---
+++ Code/courseai/feedback/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Feedback
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, QueryDict
+
+# Create your views here.
+
+
+def give_feedback(request):
+
+    try:
+        feedback_str = request.GET['feedback']
+        email = request.GET['email']
+    except:
+        res = JsonResponse({"response": "Error, please provide feedback"})
+        return HttpResponseBadRequest(res)
+    feedback = Feedback(text = feedback_str, email = email)
+    feedback.save()
+
+    return JsonResponse({"response": "success"})
+
