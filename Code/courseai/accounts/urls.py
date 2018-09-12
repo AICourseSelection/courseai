5a3de66c8ec5d3d689a698f82da66686f66129a5
Sijia Qin <u5906375@anu.edu.au>
2018-09-12 16:37:13 +1000

    fixed a small typo error

--- Code/courseai/accounts/urls.py
+++ Code/courseai/accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register_view', views.register_view, name='register'),
     path('logout_view', views.logout_view, name='logout'),
     path('degree_plan_view', views.code_view),
-    path('profile', views.get_user_profile, name="profile").
-    path('account_activate_view', views.account_activate_view, name='activate')
+    path('profile', views.get_user_profile, name="profile"),
+    # path('account_activate_view', views.account_activate_view, name='activate')
 ]
