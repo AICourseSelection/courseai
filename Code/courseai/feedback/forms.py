e0fed265c5b4eba977cd3fe898340c404e334daf
Yong Wei <u6033740@anu.edu.au>
2018-09-19 16:23:02 +1000

    Forms.py clean up

--- Code/courseai/feedback/forms.py
+++ Code/courseai/feedback/forms.py
@@ -3,23 +3,16 @@
 from django.forms import ModelForm
 from .models import Feedback
 
-# Unused
-class FeedbackForm(ModelForm):
-
-    class Meta:
-        model = Feedback
-        fields = "__all__"
-		
 # Form used for feedback		
-class ContactForm(forms.Form):
+class FeedbackForm(forms.Form):
     email = forms.EmailField(required=False)
     name = forms.CharField(required=False)
     comments = forms.CharField(widget=forms.Textarea, required=True)
 	
 	# Used to relabel
 	def __init__(self, *args, **kwargs):
-        super(ContactForm, self).__init__(*args, **kwargs)
+        super(FeedbackForm, self).__init__(*args, **kwargs)
         self.fields['name'].label = "Your Name:"
         self.fields['email'].label = "Your Email:"
         self.fields['comments'].label =
-            "Your Message For Us:"
+            "Your Message:"
