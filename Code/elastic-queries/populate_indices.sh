9fd45cf5be4cca21efa0843d87aef6046b6d946d
manalmohania <u6019071@anu.edu.au>
2018-09-22 18:16:28 +1000

    slight change to the script
    Former-commit-id: 45a34bf09918f47bc059e6106931db31edf0f187

--- Code/elastic-queries/populate_indices.sh
+++ Code/elastic-queries/populate_indices.sh
@@ -13,9 +13,10 @@ curl -X DELETE "$1:9200/majors"
 curl -X DELETE "$1:9200/minors"
 curl -X DELETE "$1:9200/specialisations"
 curl -X DELETE "$1:9200/courses"
+curl -X DELETE "$1:9200/courseupdated"
 
 
-declare -a BULKS=("@degree_bulk" "@major_updated_bulk" "@minor_updated_bulk" "@specialisation_updated_bulk"  "@courselists_bulk" "@course_updated_bulk")
+declare -a BULKS=("@course_bulk" "@degree_bulk" "@major_updated_bulk" "@minor_updated_bulk" "@specialisation_updated_bulk"  "@courselists_bulk" "@course_updated_bulk")
 
 for bulk in "${BULKS[@]}"
 do
