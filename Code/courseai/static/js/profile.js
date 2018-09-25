8a7a0e8efaada2de46b9c733ac92c6277f582386
thienbuinguyen <thien.minh.bui.nguyen@gmail.com>
2018-09-26 00:57:50 +1000

    fix footer on user page, edit user page to display full titles

--- Code/courseai/static/js/profile.js
+++ Code/courseai/static/js/profile.js
@@ -1,8 +1,13 @@
 const PLAN_DELIMITER = '|';
 const CODE_DELIMITER = '~';
 
+let degreeDict = {}; // mapping of degree codes to titles
+let majorDict = {}; // mapping of major codes to titles
+let minorDict = {}; // mapping of minor codes to titles
+let specDict = {}; // mapping of specialisation codes to titles
+
 function createDeleteBtn(code) {
-    let btn = $('<button class="btn btn-outline-danger btn-default btn-delete">Delete</button>');
+    let btn = $('<button class="btn btn-outline-danger btn-sm btn-delete">Delete</button>');
     btn.on('click', function() {
         // remove the code from the user's profile
         $.ajax({
@@ -21,7 +26,7 @@ function createDeleteBtn(code) {
 }
 
 function createLoadBtn(code, startYear, startSem) {
-    let btn = $('<button class="btn btn-outline-success btn-default btn-load">Load</button>');
+    let btn = $('<button class="btn btn-outline-success btn-sm btn-load mb-1">Load</button>');
     btn.on('click', function() {
         let codeCell = $(this).parent().prev();
         if (codeCell.length !== 0) {
@@ -33,33 +38,97 @@ function createLoadBtn(code, startYear, startSem) {
     return btn;
 }
 
+function stringifyDegreeCodes(degreeCodes) {
+    let str = "";
+    for (let i = 0; i < degreeCodes.length; i++) {
+        if (i !== 0) str += ", ";
+        str += degreeDict[degreeCodes[i]];
+    }
+    return str;
+}
+
+function stringifyMMS(mms) {
+    let str = "";
+    for (let i = 0; i < mms.length; i++) {
+        let code = mms[i].code;
+        let year = mms[i].year;
+        let type = code.substr(code.length - 3);
+        if (i !== 0) str += ", ";
+
+        if (type === 'MAJ') str += majorDict[code + year];
+        else if (type === 'MIN') str += minorDict[code + year];
+        else str += specDict[code + year];
+
+        str +=  ' (' + type + ')';
+    }
+    return str;
+}
+
+// create dictionary of degree codes to titles
 $.ajax({
-    url: '/accounts/degree_plan_view',
-    success: function(data) {
-        if (data.length > 1) {
-            let plans = data.split(PLAN_DELIMITER);
-            for (var i = 0; i < plans.length; i++)
-                plans[i] = plans[i].split(CODE_DELIMITER);
-
-            // insert degree plans into the table
-            let tableBody = $('#codes-table').find('tbody');
-            for (var i = 0; i < plans.length; i++) {
-                let obj = JSON.parse(plans[i][1]);
-                let code = plans[i][0];
-                let row = $('<tr class="d-flex">');
-                let btnsCol = $('<td class="col-2 btn-container row-fluid"/>');
-
-                // row.append('<td class="">' + plans[i][0] + '</td>');                                                 // code
-                row.append('<td class="col-2 text-center">' + obj.degrees.join(', ') + '</td>');                         // degrees
-                row.append('<td class="col-4 text-center">' + obj.trackedMMS.join(', ') + '</td>');                      // mms
-                row.append('<td class="col-2 text-center">' + 'Semester ' + obj.startSem + ' ' + obj.startYear + '</td>');   // start date
-                row.append('<td class="col-2 text-center">' + obj.created + '</td>');                                   // created
-
-                btnsCol.append(createLoadBtn(code, obj.startYear, obj.startSem));
-                btnsCol.append(createDeleteBtn(code));
-                row.append(btnsCol);
-                tableBody.append(row);
-            }
+    url: '/degree/all',
+    success: function (data) {
+        for (i in data.response) {
+            degreeDict[String(data.response[i].code)] = data.response[i].title;
         }
     }
-})
+});
+
+function mapMMSCodeToTitle(queryName, dict) {
+    return $.ajax({
+        url: '/search/' + queryName,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            for (var i = 0; i < data.response.length; i++) {
+                let code = data.response[i]._source.code;
+               let mms = data.response[i]._source.versions; 
+               for (var obj in mms) {
+                   dict[String(code) + String(obj)] = mms[obj].title;
+               }
+            }
+        }
+    });
+}
+
+function createSavedPlansTable() {
+    $.ajax({
+        url: '/accounts/degree_plan_view',
+        success: function(data) {
+            if (data.length > 1) {
+                let plans = data.split(PLAN_DELIMITER);
+                for (var i = 0; i < plans.length; i++)
+                    plans[i] = plans[i].split(CODE_DELIMITER);
+
+                // insert degree plans into the table
+                let tableBody = $('#saved-plans-table').find('tbody');
+                for (var i = 0; i < plans.length; i++) {
+                    let obj = JSON.parse(plans[i][1]);
+                    let code = plans[i][0];
+                    let row = $('<tr class="d-flex">');
+                    let btnsCol = $('<td class="col-1 btn-container text-center"/>');
+                    let textarea = $('<textarea class="plan-name-input form-control" placeholder="edit name"/>');
+
+                    let nameInput = $('<td class="col-2 name-td"/>').append(textarea);                                              
+                    row.append(nameInput);
+                    row.append('<td class="col-1 text-center">' + plans[i][0] + '</td>');                                // code
+                    row.append('<td class="col-2 text-center">' + stringifyDegreeCodes(obj.degrees) + '</td>');                  // degrees
+                    row.append('<td class="col-4 text-center">' + stringifyMMS(obj.trackedMMS) + '</td>');                       // mms
+                    row.append('<td class="col-1 text-center">' + 'Semester ' + obj.startSem + ' ' + obj.startYear + '</td>');   // start date
+                    row.append('<td class="col-1 text-center">' + obj.created + '</td>');                                       // created
+
+                    btnsCol.append(createLoadBtn(code, obj.startYear, obj.startSem));
+                    btnsCol.append(createDeleteBtn(code));
+                    row.append(btnsCol);
+                    tableBody.append(row);
+                }
+            }
+        }
+    })
+}
+
+$.when(mapMMSCodeToTitle('majors', majorDict), 
+    mapMMSCodeToTitle('minors', minorDict), 
+    mapMMSCodeToTitle('specs', specDict)).done(function() {
+        createSavedPlansTable();
+    });
