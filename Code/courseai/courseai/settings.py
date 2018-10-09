b5be46b61cfd25830fabf87e4963be104eac6500
Thomas William Hamer <u6081614>
2018-10-09 21:09:33 +1100

    added manage.py extention and docker file intergration

--- Code/courseai/courseai/settings.py
+++ Code/courseai/courseai/settings.py
@@ -113,9 +113,7 @@ else:
         'PASSWORD': 'courseai312',
         'HOST': 'localhost',
         'PORT': '5432',
-            },
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
         }
 
 DATABASES = databaseConfig
